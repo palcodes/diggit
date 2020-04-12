811cff13c2153a22e070f3436532445aaa333ea7
Aayush Pal <47224106+pal-codes@users.noreply.github.com>
2020-04-12 22:17:11 +0530

    refactored stuff

---
+++ lib/home.dart
@@ -0,0 +1,9 @@
+import 'package:diggit/main.dart';
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
