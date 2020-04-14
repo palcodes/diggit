7001c1e0d92f47bf21fd75512b980482cdd549a5
Aayush Pal <47224106+pal-codes@users.noreply.github.com>
2020-04-15 01:09:03 +0530

    refactored

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,4 @@
-import 'package:diggit/model.dart';
+import 'package:diggit/models/meetsModel.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:http/http.dart' as http;
