e392e80d7b5fb0c95c7feac95f2e8c6714d4661d
Aayush Pal <47224106+pal-codes@users.noreply.github.com>
2020-04-15 16:17:00 +0530

    Major UI milestone

--- lib/home.dart
+++ lib/home.dart
@@ -37,18 +37,128 @@ class _MyHomePageState extends State<MyHomePage> {
   void initState() {
     // TODO: implement initState
     super.initState();
-    reqsend();
+    // reqsend();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: ListView(
-          children: <Widget>[
-            Text('Lets see'),
-          ],
-        ),
+      body: ListView(
+        children: <Widget>[
+          Stack(
+            alignment: Alignment.center,
+            overflow: Overflow.visible,
+            children: [
+              Container(
+                color: Color.fromRGBO(30, 33, 37, 1),
+                height: 370,
+              ),
+              Positioned(
+                top: 330,
+                height: 150,
+                child: Container(
+                  padding: EdgeInsets.all(15),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Bio',
+                        textAlign: TextAlign.left,
+                        style: GoogleFonts.poppins(
+                            fontWeight: FontWeight.w600, fontSize: 20),
+                      ),
+                      Text(
+                        'I am a Mumbai based Software Engineer, looking for new opportunities.',
+                        style: GoogleFonts.poppins(
+                            fontSize: 16, fontWeight: FontWeight.w300),
+                      )
+                    ],
+                  ),
+                  width: 350,
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey[800],
+                          blurRadius: 30,
+                        )
+                      ],
+                      borderRadius: BorderRadius.circular(12)),
+                ),
+              ),
+              Positioned(
+                left: 42,
+                top: 85,
+                child: Container(
+                  height: 85,
+                  width: 85,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(12),
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.black, blurRadius: 25, spreadRadius: 10)
+                    ],
+                  ),
+                ),
+              ),
+              Positioned(
+                left: 40,
+                bottom: 120,
+                child: Text(
+                  'Hi I am,',
+                  style: GoogleFonts.poppins(color: Colors.white, fontSize: 30),
+                ),
+              ),
+              Positioned(
+                left: 40,
+                bottom: 75,
+                child: Text(
+                  'Calvin Gomez',
+                  style: GoogleFonts.poppins(
+                      color: Colors.white,
+                      fontSize: 35,
+                      fontWeight: FontWeight.w700),
+                ),
+              ),
+            ],
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 150, left: 45),
+            child: Text(
+              'People',
+              style: GoogleFonts.poppins(
+                  fontWeight: FontWeight.w600, fontSize: 20),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 15, left: 35, right: 35),
+            height: 80,
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Text('Followers',
+                    style: GoogleFonts.poppins(
+                        fontSize: 18, fontWeight: FontWeight.w500)),
+                VerticalDivider(
+                  indent: 8,
+                  endIndent: 8,
+                  width: 2,
+                  color: Colors.grey[500],
+                ),
+                Text('Following',
+                    style: GoogleFonts.poppins(
+                        fontSize: 18, fontWeight: FontWeight.w500)),
+              ],
+            ),
+            decoration: BoxDecoration(
+                color: Colors.white,
+                boxShadow: [BoxShadow(color: Colors.grey[500], blurRadius: 20)],
+                borderRadius: BorderRadius.circular(12)),
+          ),
+        ],
       ),
     );
   }
