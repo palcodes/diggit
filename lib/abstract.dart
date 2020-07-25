b3b9c02e230b46c6b5d0749e2fdf6a46bcb72378
Aayush Pal <ap.aayushpal@gmail.com>
2020-07-25 20:21:39 +0530

    Now using dio for http calls 🙌🏽

--- lib/abstract.dart
+++ lib/abstract.dart
@@ -2,38 +2,46 @@ import 'package:diggit/models/meetsModel.dart';
 import 'package:diggit/models/reposModel.dart';
 import 'package:diggit/models/userModel.dart';
 
-import 'package:http/http.dart' as http;
+// import 'package:http/http.dart' as http;
+import 'package:dio/dio.dart';
 
-import 'dart:convert';
+// import 'dart:convert';
 import 'dart:math';
 
 Future<Meets> getTheUser() async {
   try {
-    http.Response response =
-        await http.get('https://api.github.com/users?per_page=800');
-    List<User> users = (json.decode(response.body) as List)
-        .map((i) => User.fromJson(i))
-        .toList();
+    Dio dio = new Dio();
+    Response responseForUser;
+    Response responseForMeet;
     Random random = new Random();
+    // http.Response response =
+    //     await http.get('https://api.github.com/users?per_page=800');
+    responseForUser =
+        await dio.get('https://api.github.com/users?per_page=800');
+    List<User> users =
+        (responseForUser.data as List).map((i) => User.fromJson(i)).toList();
     User theUser = users[random.nextInt(users.length)];
     print("NEW MEET FOUND -> " + theUser.url);
-    http.Response resInfo = await http.get(theUser.url);
-    Meets foundUser = Meets.fromJson(json.decode(resInfo.body));
-    print("${foundUser.name} \n ${foundUser.bio}");
-    return foundUser;
+    // http.Response resInfo = await http.get(theUser.url);
+    responseForMeet = await dio.get(theUser.url);
+    Meets foundMeet = Meets.fromJson(responseForMeet.data);
+    print("${foundMeet.name} \n ${foundMeet.bio}");
+    return foundMeet;
   } catch (e) {
     print('CAUGHT ERROR IN getTheUser:' + e.toString());
     return null;
   }
 }
 
-Future<List<Repos>> getRepos(Meets foundUser) async {
+Future<List<Repos>> getRepos(Meets foundMeet) async {
   try {
-    print('FOUND USER: ' + foundUser.url.toString());
-    http.Response resRepo = await http.get(foundUser.reposUrl);
-    List<Repos> repos = (json.decode(resRepo.body) as List)
-        .map((i) => Repos.fromJson(i))
-        .toList();
+    Response responseForRepo;
+    Dio dio = new Dio();
+    print('FOUND USER: ' + foundMeet.url.toString());
+    // http.Response resRepo = await http.get(foundUser.reposUrl);
+    responseForRepo = await dio.get(foundMeet.reposUrl);
+    List<Repos> repos =
+        (responseForRepo.data as List).map((i) => Repos.fromJson(i)).toList();
     return repos;
   } catch (e) {
     print('CAUGHT ERROR IN getRepos:' + e.toString());
